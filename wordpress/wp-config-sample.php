f9efeaa8c695665463ca836ce8fc6ce9e1f85f83
DiscoDino01 <159913791+DiscoDino01@users.noreply.github.com>
2024-12-04 11:09:35 +0800

    reset

--- wordpress/wp-config-sample.php
+++
@@ -1,4 +0,0 @@
-define( 'DB_NAME', 'wordpress' );
-define( 'DB_USER', 'wpuser' );
-define( 'DB_PASSWORD', '1971' );
-define( 'DB_HOST', 'localhost' );
--- wordpress/wp-config.php
+++
@@ -1,4 +0,0 @@
-define( 'DB_NAME', 'wordpress' );
-define( 'DB_USER', 'wpuser' );
-define( 'DB_PASSWORD', 'wp_pass123' );
-define( 'DB_HOST', 'localhost' );
